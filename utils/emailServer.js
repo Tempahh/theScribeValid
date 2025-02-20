b302c09d1333187e95100f3a86c7ba5d4b0d9805
Prosper <prosperorjiogo1999@gmail.com>
2025-02-20 08:25:25 +0100

    [refactor]: Remove hardcoded email credentials and clean up email sending logic

--- utils/emailServer.js
+++ utils/emailServer.js
@@ -6,24 +6,20 @@ const transporter = nodemailer.createTransport({
     port: 587,
     secure: false, // Change to your email provider if needed
     auth: {
-        user: process.env.EMAIL_USER || 'plance991@gmail.com',  // Your email address
-        pass: process.env.EMAIL_PASS || 'hxwzzchrsmqbfpna'  // Your email password or app password
+        user: process.env.EMAIL_USER, // Your email address
+        pass: process.env.EMAIL_PASS  // Your email password or app password
     }
 });
-
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
 
 // Function to send email
 const sendEmail = async ({to, subject, text}) => {
     try {
         const mailOptions = {
-            from: process.env.EMAIL_USER || 'plance991@gmail.com', // Your email address
+            from: process.env.EMAIL_USER, // Your email address
             to,
             subject,
             text
         };
-
         
         const info = await transporter.sendMail(mailOptions);
         console.log("Message sent: %s", info.response);
